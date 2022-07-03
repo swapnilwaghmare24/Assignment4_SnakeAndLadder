bf3650be825faa0975c8ed7c498b589304276868
swapnil.waghmare24 <iamswapnilengg@gmail.com>
2022-07-03 21:37:14 +0530

    getting dievalue

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,10 +2,16 @@ public class SnakeAndLadder
 {
 	static int StartPosition=0;
 	static int PlayerPosition=0;
+	static int dieValue()
+	{
+		int dieValue=(int)(Math.random()*6)+1;
+		return dieValue;
+	}
 public static void main(String[] args)
 {
 System.out.println("Welcome to snake and ladder program");
 System.out.println("Start Position "+StartPosition);
 System.out.println("Player Position "+PlayerPosition);
+System.out.println("dieValue "+dieValue());
 }
 }
